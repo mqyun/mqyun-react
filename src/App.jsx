386beeb89c279e7300428b6730b46dadeeb0085a
mqyun <17855831290@163.com>
2018-11-06 20:02:46 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.styl';
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Login from './page/Login/Login.jsx'
+
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+          <div className="container">
+            <Route exact path="/" component={Login} />
+          </div>
+        </BrowserRouter>
+    );
+  }
+}
+
+export default App;
